05cf10dccda2837b68390ebad82a8d38589f281e
ezopus <133430790+ezopus@users.noreply.github.com>
2024-01-22 14:00:58 +0200

    fixed solution for judge

--- 03.ObjectsAndClasses/L09.Songs.js
+++ 03.ObjectsAndClasses/L09.Songs.js
@@ -1,14 +1,13 @@
 "use strict";
 
-class Song {
-  constructor(type, name, time) {
-    this.type = type;
-    this.name = name;
-    this.time = time;
-  }
-}
-
 function songFilter(songs) {
+  class Song {
+    constructor(type, name, time) {
+      this.type = type;
+      this.name = name;
+      this.time = time;
+    }
+  }
   const number = songs.shift();
   const songType = songs.pop();
 
@@ -27,6 +26,12 @@ function songFilter(songs) {
   console.log(result);
 }
 
-songFilter([3, "favourite_DownTown_3:14", "favourite_Kiss_4:16", "favourite_Smooth Criminal_4:01", "favourite"]);
+songFilter([
+  3,
+  "favourite_DownTown_3:14",
+  "favourite_Kiss_4:16",
+  "favourite_Smooth Criminal_4:01",
+  "favourite",
+]);
 
 songFilter([2, "like_Replay_3:15", "ban_Photoshop_3:48", "all"]);
