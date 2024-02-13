87fa57c78c363b6a8427314623a471fafca48d6d
ezopus <133430790+ezopus@users.noreply.github.com>
2024-02-13 14:49:07 +0200

    minor updates

--- 03.ObjectsAndClasses/M06.BrowserHistory.js
+++ 03.ObjectsAndClasses/M06.BrowserHistory.js
@@ -19,10 +19,7 @@ function browserHistory(obj, arr) {
             }
         } else if (command === "Clear") {
             tabs = [];
-
-
             recent = [];
-
             logs = [];
         }
     });
@@ -33,36 +30,36 @@ function browserHistory(obj, arr) {
     console.log(`Browser Logs: ${logs.join(", ")}`);
 }
 
-// browserHistory(
-//   {
-//     "Browser Name": "Google Chrome",
-//     "Open Tabs": ["Facebook", "YouTube", "Google Translate"],
-//     "Recently Closed": ["Yahoo", "Gmail"],
-//     "Browser Logs": [
-//       "Open YouTube",
-//       "Open Yahoo",
-//       "Open Google Translate",
-//       "Close Yahoo",
-//       "Open Gmail",
-//       "Close Gmail",
-//       "Open Facebook",
-//     ],
-//   },
-//   ["Close Facebook", "Open StackOverFlow", "Open Google", "Clear History and Cache"]
-// );
-
 browserHistory(
-    {
-        "Browser Name": "Mozilla Firefox",
-        "Open Tabs": ["YouTube"],
-        "Recently Closed": ["Gmail", "Dropbox"],
-        "Browser Logs": [
-            "Open Gmail",
-            "Close Gmail",
-            "Open Dropbox",
-            "Open YouTube",
-            "Close Dropbox",
-        ],
-    },
-    ["Open Wikipedia", "Clear History and Cache", "Open Twitter"]
+  {
+    "Browser Name": "Google Chrome",
+    "Open Tabs": ["Facebook", "YouTube", "Google Translate"],
+    "Recently Closed": ["Yahoo", "Gmail"],
+    "Browser Logs": [
+      "Open YouTube",
+      "Open Yahoo",
+      "Open Google Translate",
+      "Close Yahoo",
+      "Open Gmail",
+      "Close Gmail",
+      "Open Facebook",
+    ],
+  },
+  ["Close Facebook", "Open StackOverFlow", "Open Google", "Clear History and Cache"]
 );
+
+// browserHistory(
+//     {
+//         "Browser Name": "Mozilla Firefox",
+//         "Open Tabs": ["YouTube"],
+//         "Recently Closed": ["Gmail", "Dropbox"],
+//         "Browser Logs": [
+//             "Open Gmail",
+//             "Close Gmail",
+//             "Open Dropbox",
+//             "Open YouTube",
+//             "Close Dropbox",
+//         ],
+//     },
+//     ["Open Wikipedia", "Clear History and Cache", "Open Twitter"]
+// );
--- 03.ObjectsAndClasses/M09.Armies.js
+++ 03.ObjectsAndClasses/M09.Armies.js
@@ -1,77 +1,78 @@
 function armyCommands(arr) {
-  class General {
-    constructor(name) {
-      this.name = name;
-      this.armies = {};
-      this.totalArmy = 0;
+    class General {
+        constructor(name) {
+            this.name = name;
+            this.armies = {};
+            this.totalArmy = 0;
+        }
     }
-  }
-  const result = arr.reduce((acc, curr) => {
-    if (curr.includes("arrives")) {
-      const [leader, _] = curr.split(" arrives");
-      acc.push(new General(leader));
-    } else if (curr.includes("defeated")) {
-      const [leaderDefeated, _] = curr.split(" defeated");
-      const deleteIndex = acc.findIndex((g) => g.name === leaderDefeated);
-      acc.splice(deleteIndex, 1);
-    } else if (curr.includes(":")) {
-      let [leader, armyName, armyCount] = curr.split(/[:,]/g);
-      armyName = armyName.trim();
-      armyCount = Number(armyCount);
 
-      const currentGeneral = acc.find((g) => g.name === leader);
-      if (currentGeneral) {
-        currentGeneral.armies[armyName] = armyCount;
-        currentGeneral.totalArmy += Number(armyCount);
-      }
-    } else if (curr.includes("+")) {
-      let [armyName, armyCount] = curr.split(" + ");
-      armyCount = Number(armyCount);
+    const result = arr.reduce((acc, curr) => {
+        if (curr.includes("arrives")) {
+            const [leader, _] = curr.split(" arrives");
+            acc.push(new General(leader));
+        } else if (curr.includes("defeated")) {
+            const [leaderDefeated, _] = curr.split(" defeated");
+            const deleteIndex = acc.findIndex((g) => g.name === leaderDefeated);
+            acc.splice(deleteIndex, 1);
+        } else if (curr.includes(":")) {
+            let [leader, armyName, armyCount] = curr.split(/[:,]/g);
+            armyName = armyName.trim();
+            armyCount = Number(armyCount);
 
-      let currentGeneral = acc.find((g) => g.armies[armyName]);
-      if (currentGeneral) {
-        currentGeneral.armies[armyName] += armyCount;
-        currentGeneral.totalArmy += Number(armyCount);
-      }
-    }
-    return acc;
-  }, []);
+            const currentGeneral = acc.find((g) => g.name === leader);
+            if (currentGeneral) {
+                currentGeneral.armies[armyName] = armyCount;
+                currentGeneral.totalArmy += Number(armyCount);
+            }
+        } else if (curr.includes("+")) {
+            let [armyName, armyCount] = curr.split(" + ");
+            armyCount = Number(armyCount);
 
-  result
-    .sort((a, b) => b.totalArmy - a.totalArmy)
-    .forEach((leader) => {
-      console.log(`${leader.name}: ${leader.totalArmy}`);
-      Object.keys(leader.armies)
-        .sort((x, y) => leader.armies[y] - leader.armies[x])
-        .forEach((a) => {
-          console.log(`>>> ${a} - ${leader.armies[a]}`);
+            let currentGeneral = acc.find((g) => g.armies[armyName]);
+            if (currentGeneral) {
+                currentGeneral.armies[armyName] += armyCount;
+                currentGeneral.totalArmy += Number(armyCount);
+            }
+        }
+        return acc;
+    }, []);
+
+    result
+        .sort((a, b) => b.totalArmy - a.totalArmy)
+        .forEach((leader) => {
+            console.log(`${leader.name}: ${leader.totalArmy}`);
+            Object.keys(leader.armies)
+                .sort((x, y) => leader.armies[y] - leader.armies[x])
+                .forEach((a) => {
+                    console.log(`>>> ${a} - ${leader.armies[a]}`);
+                });
         });
-    });
 }
 
 armyCommands([
-  "Rick Burr arrives",
-  "Fergus: Wexamp, 30245",
-  "Rick Burr: Juard, 50000",
-  "Findlay arrives",
-  "Findlay: Britox, 34540",
-  "Wexamp + 6000",
-  "Juard + 1350",
-  "Britox + 4500",
-  "Porter arrives",
-  "Porter: Legion, 55000",
-  "Legion + 302",
-  "Rick Burr defeated",
-  "Porter: Retix, 3205",
+    "Rick Burr arrives",
+    "Fergus: Wexamp, 30245",
+    "Rick Burr: Juard, 50000",
+    "Findlay arrives",
+    "Findlay: Britox, 34540",
+    "Wexamp + 6000",
+    "Juard + 1350",
+    "Britox + 4500",
+    "Porter arrives",
+    "Porter: Legion, 55000",
+    "Legion + 302",
+    "Rick Burr defeated",
+    "Porter: Retix, 3205",
 ]);
 
 armyCommands([
-  "Rick Burr arrives",
-  "Findlay arrives",
-  "Rick Burr: Juard, 1500",
-  "Wexamp arrives",
-  "Findlay: Wexamp, 34540",
-  "Wexamp + 340",
-  "Wexamp: Britox, 1155",
-  "Wexamp: Juard, 43423",
+    "Rick Burr arrives",
+    "Findlay arrives",
+    "Rick Burr: Juard, 1500",
+    "Wexamp arrives",
+    "Findlay: Wexamp, 34540",
+    "Wexamp + 340",
+    "Wexamp: Britox, 1155",
+    "Wexamp: Juard, 43423",
 ]);
