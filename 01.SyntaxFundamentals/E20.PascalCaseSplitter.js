a059051752e6f7c774525d9c1890223a69582f06
ezopus <133430790+ezopus@users.noreply.github.com>
2024-03-01 21:55:33 +0200

    simplified solution

--- 01.SyntaxFundamentals/E20.PascalCaseSplitter.js
+++ 01.SyntaxFundamentals/E20.PascalCaseSplitter.js
@@ -2,14 +2,11 @@ function pascalCaseSplitter(word) {
   let sentence = word.split("");
   for (let i = 1; i < sentence.length; i++) {
     if (sentence[i].charCodeAt(0) >= 65 && sentence[i].charCodeAt(0) <= 90) {
-      sentence.splice(i, 0, " ");
+      sentence.splice(i, 0, ", ");
       i++;
     }
   }
-  console.log(sentence.join("").split(" ").join(", "));
-
-  // let sentence = word.match(/[A-Z][a-z]+/g);
-  // console.log(sentence.join(", "));
+  console.log(sentence.join(""));
 }
 
 pascalCaseSplitter("HelloWorld");
