047e78c4c6e4c209920137e452b1ac9ce47f3006
ezopus <133430790+ezopus@users.noreply.github.com>
2024-03-14 11:59:47 +0200

    simplified

--- 03.ObjectsAndClasses/E07.OddOccurences.js
+++ 03.ObjectsAndClasses/E07.OddOccurences.js
@@ -1,21 +1,20 @@
 function printOddOccurences(str) {
-  const result = str
-    .toLowerCase()
-    .split(" ")
-    .reduce((acc, curr) => {
-      if (!acc.hasOwnProperty(curr)) {
-        acc[curr] = 1;
-      } else {
-        acc[curr]++;
-      }
-      return acc;
-    }, {});
+    const result = str
+        .toLowerCase()
+        .split(" ")
+        .reduce((acc, curr) => {
+            if (!acc.hasOwnProperty(curr)) {
+                acc[curr] = 0;
+            }
+            acc[curr]++;
+            return acc;
+        }, {});
 
-  console.log(
-    Object.keys(result)
-      .filter((word) => result[word] % 2 !== 0)
-      .join(" ")
-  );
+    console.log(
+        Object.keys(result)
+            .filter((word) => result[word] % 2 !== 0)
+            .join(" ")
+    );
 }
 
 printOddOccurences("Java C# Php PHP Java PhP 3 C# 3 1 5 C#");
